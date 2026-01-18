ef16ae5af46945330e5692f3be7274ffdefcded1
SimeonChifligarov <simeon.chifligarov@gmail.com>
2026-01-18 13:18:23 +0200

    add main.py guess_that_number

---
+++ 08_guess_that_number/main.py
@@ -0,0 +1,39 @@
+from typing import Final
+import random
+
+# Add parameters
+LOWER_LIMIT: Final[int] = 0
+UPPER_LIMIT: Final[int] = 100
+random_number: int = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+
+
+# Easy printing for what the bot says
+def bot_message(msg: str) -> None:
+    print(f'Bot: {msg}')
+
+
+# Intro message
+bot_message('Welcome to GuessThatNumber™!')
+bot_message(f'Guess a number between {LOWER_LIMIT} & {UPPER_LIMIT}.')
+
+# Infinite loop until user guesses
+while True:
+    # Validate user input so it doesn't crash the program
+    try:
+        user_guess: int = int(input('You: '))
+    except ValueError as e:
+        bot_message(f'{e}, please only use numbers.')
+        continue
+
+    # Check user input against the number guessed
+    if user_guess > random_number:
+        bot_message('The number is lower.')
+    elif user_guess < random_number:
+        bot_message('The number is higher.')
+    else:
+        bot_message('You guessed correctly! You win!')
+        break
+
+# Homework:
+# 1. Add a score keeping mechanism that includes the amount of tries it took to guess
+# the correct number.
